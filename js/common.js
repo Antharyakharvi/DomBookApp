e39ca39f8e97969136b3c9fbfde4b63983fc53a2
Antharyakharvi <antharyakharvi@gmail.com>
2024-12-28 14:47:53 +0530

    am

---
+++ js/common.js
@@ -0,0 +1,11 @@
+const BASE_URL = "https://dom-book-backend.glitch.me/books";
+
+function renderNavbar() {
+  document.write(`
+    <nav>
+      <a href="index.html">Home</a>
+      <a href="admin.html">Admin</a>
+      <a href="books.html">Books</a>
+    </nav>
+  `);
+}
